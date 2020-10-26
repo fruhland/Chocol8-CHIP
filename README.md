31d4a39be62080b5cf191f5e772b7b766e55e060
Fabian Ruhland <Fabian.Ruhland@uni-duesseldorf.de>
2020-10-26 16:09:02 +0100

    Update dependencies; Update logo and README; swing-app: Refactor CLI

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # Chocol8 CHIP
 
 <p align="center">
-  <img src="logo.svg" height="200px">
+  <img src="media/logo.svg" height="200px">
 </p>
 
 <p align="center">
-  <a href="https://travis-ci.com/faruh100/Chocol8-CHIP"><img src="https://travis-ci.com/faruh100/Chocol8-CHIP.svg?branch=master"></a>
+  <a href="https://travis-ci.com/fruhland/Chocol8-CHIP"><img src="https://travis-ci.com/fruhland/Chocol8-CHIP.svg?branch=master"></a>
   <a href="https://openjdk.java.net/projects/jdk8/"><img src="https://img.shields.io/badge/java-8-blue.svg"></a>
-  <a href="https://openjdk.java.net/projects/jdk/11/"><img src="https://img.shields.io/badge/java-11-blue.svg"></a>
   <a href="https://github.com/hhu-bsinfo/observatory/blob/master/LICENSE"><img src="https://img.shields.io/badge/license-GPLv3-orange.svg"></a>
 </p>
 
--- swing-app/src/main/java/one/ruhland/chocol8/swing/Application.java
+++ swing-app/src/main/java/one/ruhland/chocol8/swing/Application.java
@@ -1,17 +1,61 @@
 package one.ruhland.chocol8.swing;
 
-import one.ruhland.chocol8.swing.commands.Root;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import one.ruhland.chocol8.chip.DefaultSound;
+import one.ruhland.chocol8.chip.Machine;
 import picocli.CommandLine;
 
-public class Application {
+@CommandLine.Command(
+    name = "chocol8",
+    description = "",
+    showDefaultValues = true,
+    separator = " ")
+public class Application implements Runnable {
 
+    @CommandLine.Option(
+        names = {"-o", "--open" },
+        description = "The CHIP-8 ROM to open")
+    private String romPath;
+
+    @CommandLine.Option(
+        names = {"-f", "--frequency" },
+        description = "The CPU frequency")
+    private int cpuFrequency = 1000;
+
+    @Override
+    public void run() {
+        Machine machine;
+
+        try {
+            machine = new Machine(SwingGraphics.class, DefaultSound.class, SwingKeyboard.class);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        var window = new MainWindow(machine);
+        window.setVisible(true);
+
+        machine.getCpu().getClock().setFrequency(cpuFrequency);
+
+        if(romPath != null) {
+            try {
+                machine.loadProgram(romPath);
+                window.setTitle(window.getTitle() + " - " + new File(romPath).getName());
+                machine.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static void main(String... args) {
-        CommandLine cli = new CommandLine(new Root());
+        Machine.printBanner();
+
+        CommandLine cli = new CommandLine(new Application());
         cli.setCaseInsensitiveEnumValuesAllowed(true);
-        cli.parseWithHandlers(
-                new CommandLine.RunAll().useOut(System.out),
-                CommandLine.defaultExceptionHandler().useErr(System.err),
-                args);
+        cli.execute(args);
     }
 }
